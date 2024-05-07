b93251e604feae7221bd01835432d916d6d318fb
Ricardo Bañuelos De La Torre <47264662+RicardoBanuelos@users.noreply.github.com>
2024-05-07 15:06:10 -0700

    updated zombie textures to a single spritesheet

--- SFML_Learning/Headers/TextureLoader/TextureStrings.h
+++ SFML_Learning/Headers/TextureLoader/TextureStrings.h
@@ -7,6 +7,6 @@ namespace TextureStrings {
 	const std::unordered_map<std::string, std::string> texturePaths = {
 		{"Player", "Assets/Player/player.png"},
 		{"Bullet", "Assets/Bullet/bullet.png"},
-		{"Zombie", "Assets/Zombie/Idle/skeleton-idle_0.png"}
+		{"Zombie", "Assets/Zombie/idle.png"}
 	};
 }
