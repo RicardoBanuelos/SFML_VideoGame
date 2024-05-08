6d15d2decb55f18e722959ce9bc013d288ecf6ac
Ricardo Bañuelos De La Torre <47264662+RicardoBanuelos@users.noreply.github.com>
2024-05-08 14:18:15 -0700

    fixed textures

--- SFML_Learning/Headers/TextureLoader/TextureData.h
+++ SFML_Learning/Headers/TextureLoader/TextureData.h
@@ -7,19 +7,13 @@ namespace TextureData {
 
 	enum TextureID {
 		TID_PLAYER,
-		TID_PLAYER_IDLE,
 		TID_BULLET,
 		TID_ZOMBIE,
-		TID_ZOMBIE_WALK_SHEET,
-		TID_ZOMBIE_ATTACK_SHEET,
 	};
 
 	const std::unordered_map<TextureID, std::string> texturePaths = {
-		{TID_PLAYER, "Assets/Player/player_2.png"},
-		{TID_PLAYER_IDLE, "Assets/Player/player.png"},
+		{TID_PLAYER, "Assets/Player/player.png"},
 		{TID_BULLET, "Assets/Bullet/bullet.png"},
 		{TID_ZOMBIE, "Assets/Zombie/zombie.png"},
-		{TID_ZOMBIE_WALK_SHEET, "Assets/Zombie/walk.png"},
-		{TID_ZOMBIE_ATTACK_SHEET, "Assets/Zombie/attack.png"},
 	};
 }
